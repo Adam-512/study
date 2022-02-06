95795ebaeb16c5e833263fcce01c5d8e4378b41c
Adam-512 <90313603+Adam-512@users.noreply.github.com>
2022-02-06 14:36:17 +0800

    补充递归

--- js/recursion.test.js
+++ js/recursion.test.js
@@ -1,9 +1,23 @@
 //array fill
 function fill(arr, value, res = []) {
-  if (res.length < arr.length) {
-    res.push(value);
-    return fill(arr, value, res);
-  }
-  return res;
+  if (arr.length === res.length) return res;
+  res.push(value);
+  return fill(arr, value, res);
 }
-console.log(fill(new Array(6), 1));
+//console.log(fill(new Array(6), 1));
+
+//阶乘
+function multiple(n, total = 1) {
+  if (n === 1) return total;
+  return n * multiple(n - 1, total);
+}
+
+//console.log(multiple(4))
+
+//累加
+function sum(n, total = 0) {
+  if (n === 0) return total;
+  return n + sum(n - 1, total);
+}
+
+console.log(sum(3));
