2dc0e0d8ffe7e005120c3e7056ff80ee35e4a136
chenhui <ch512006634@gmail.com>
2022-02-12 18:00:55 +0800

    add util debounce test

---
+++ js/utils.test.ts
@@ -0,0 +1,40 @@
+function debounce(fn: Function, delay: number) {
+  let timer: any;
+  return (...args: any) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.apply(args);
+    }, delay);
+  };
+}
+
+function throttle(fn: Function, freq: number) {
+  let time = 0;
+  return (...args: any) => {
+    const now = Date.now();
+    if (now - time > freq) {
+      fn.apply(args);
+      time = now;
+    }
+  };
+}
+
+const t = debounce(() => {
+  console.log(1);
+}, 5000);
+
+// t();
+// t();
+// setTimeout(() => {
+//   t();
+// }, 1000);
+
+const t2 = throttle(() => {
+  console.log("t2");
+}, 1000);
+t2();
+t2();
+t2();
+t2();
+t2();
+t2();
