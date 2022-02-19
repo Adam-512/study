6ee7d966d61b1c19099284b08771385c75b69cd0
Adam-512 <90313603+Adam-512@users.noreply.github.com>
2022-02-19 11:54:04 +0800

    pass test: Promise.all

---
+++ promise/promise.all.spec.js
@@ -0,0 +1,46 @@
+/**
+ * javascript comment
+ * @Author: chenhui
+ * @Param: { Array | Map | Set}  iterables  可迭代对象
+ * @Return:  { Promise<Array> | any }
+ */
+const Alldone = function (iterables) {
+  return new Promise((resolve, reject) => {
+    let result = new Array(iterables.length);
+    let counter = 0;
+    //注意使用块级作用域let
+    for (let i = 0; i < iterables.length; i++) {
+      let p = iterables[i];
+      if (p instanceof Promise) {
+        //因promise的先后顺序不确定，以参数顺序排列结果
+        p.then((value) => {
+          result[i] = value;
+          counter++;
+          console.log(value);
+          console.log("counter: ", counter);
+          if (counter === iterables.length) {
+            resolve(result);
+          }
+        }, reject);
+      } else {
+        result[i] = p;
+      }
+    }
+  });
+};
+
+function main() {
+  const p1 = new Promise((resolve) =>
+    setTimeout(() => {
+      resolve(1);
+    }, 2000)
+  );
+  const p2 = new Promise((resolve) =>
+    setTimeout(() => {
+      resolve(2);
+    }, 3000)
+  );
+  const rp = Alldone([p1, p2]);
+  rp.then((v) => console.log(v));
+}
+main();
